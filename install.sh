97d778b4c43dafc0cc522472683928a2520b543f
tester <joaopedrojacob88@gmail.com>
2022-07-26 23:45:43 +0100

    Updated install.sh for new dependencies. Updated other packages features

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ echo "Installing Rover Stack dependencies..."
 echo "This may take as much as rover speed on Mars..."
 sleep 7
 
+sudo apt-get update
+
 sudo apt-get install ros-melodic-octomap*
 
 sudo apt-get install ros-melodic-costmap-2d*
@@ -29,6 +31,28 @@ sudo apt-get install ros-melodic-controlller*
 sudo apt-get install ros-melodic-velocity-*
 
 sudo apt-get install ros-melodic-rtabmap*
+
+sudo apt-get install python-seaborn
+
+sudo apt-get install ros-melodic-rospkg*
+
+sudo apt-get install python-numpy
+
+sudo apt-get install ros-melodic-ros-numpy 
+
+sudo apt-get install python3
+
+sudo apt-get install python3-catkin-pkg-modules
+
+sudo apt-get install python3-rospkg-modules
+
+sudo apt install python3-dev python3-pip python3-venv
+
+python3 -m pip install --upgrade pip
+
+python3 -m pip install --upgrade tensorflow -U "protobuf<4.21.0"
+
+python3 -m pip install tensorflow-object-detection-api -U "protobuf<4.21.0"
 
 echo "Making sure all screws are well-tightened..."
 sleep 5
